3fd7016b26547948758b28ef8cf4052801afc997
DESKTOP-FR144OA\Anubis <adamas87@gmail.com>
2025-11-05 16:51:27 +0800

    'Fix'

--- task.sql
+++ task.sql
@@ -1,12 +1,7 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB;
 ALTER TABLE `ShopDB`.`Customers`
-ADD UNIQUE INDEX `idx_customers_email` (`Email` ASC) VISIBLE;
+ADD UNIQUE INDEX `idx_email` (`Email` ASC) VISIBLE;
 
 ALTER TABLE `ShopDB`.`Products`
-ADD INDEX `idx_products_name` (`Name` ASC) VISIBLE;
-ADD FULLTEXT INDEX `ft_products_name_desc` (`Name`, `Description`) VISIBLE;
-
-ALTER TABLE `ShopDB`.`Orders`
-ADD INDEX `idx_orders_customerid` (`CustomerID` ASC) INVISIBLE,
-ADD INDEX `idx_orders_date` (`Date` ASC) VISIBLE;
+ADD INDEX `idx_name` (`Name` ASC) VISIBLE;
